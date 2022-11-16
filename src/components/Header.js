d1486d82c846dc8c0adef8b2bf32b3b30b29a164
dansiecrown <dansiecrown@gmail.com>
2022-11-16 10:54:46 +0100

    style: added responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 export default function Header({ showToggle }) {
 
 
+    const [showHam, setShowHam] = React.useState(false);
+
+
+    const toggleHam = () => {
+        setShowHam(!showHam);
+
+    }
+
 
     return (
         <div className="header">
@@ -19,9 +27,31 @@ export default function Header({ showToggle }) {
                 <a className='nav-link' href="#">Community</a>
             </nav>
 
+            {
+                showHam ?
+                    <nav className="nav-mobile">
+                        <button onClick={() => toggleHam()} ><img src='./assets/close.png' alt='close' /></button>
+                        <Link className='nav-link' to={'/'}>Home</Link>
+                        <Link className='nav-link' to={"/stay"}>Place to stay</Link>
+                        <a className='nav-link' href="#">NFTs</a>
+                        <a className='nav-link' href="#">Community</a>
+                    </nav> : null
+            }
+
+
+
             <div className="buttons">
                 <button className="btn" onClick={() => showToggle()}>Connect Wallet</button>
             </div>
+
+            <div className="hamburger">
+                <button onClick={() => toggleHam()}>
+                    <span className="material-symbols-outlined" >
+                        menu
+                    </span>
+                </button>
+            </div>
+
         </div>
     )
 }
